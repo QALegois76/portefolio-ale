3ffb82960fd9edaa6738ad2058d689cffd31f271
Antoine LEGOIS <antoine.legois@altimet.fr>
2025-01-17 11:16:42 +0100

    init commit

---
+++ src/app/formation-component/formation-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formation-component',
+  imports: [],
+  templateUrl: './formation-component.component.html',
+  styleUrl: './formation-component.component.css'
+})
+export class FormationComponentComponent {
+
+}
